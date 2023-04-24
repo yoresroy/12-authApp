0f47277811cc8bdc127e94d0fd0a1f28e3b92396
yoresroy <yor_27@hotmail.com>
2023-04-24 00:07:33 -0600

    Add - Routing de la aplicación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'auth',
+    loadChildren : () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path : 'dashboard',
+    loadChildren : () => import('./protected/protected.module').then(m => m.ProtectedModule)
+  },
+  {
+    path : '**',
+    redirectTo : 'auth'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
